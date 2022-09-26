d6ea2d6c82163fe2c49827e4db553ead7493ffd7
Jaroslaw Dobromilski <jaroslaw.dobromilski@gmail.com>
2022-09-26 16:27:21 +0100

    gist-reader.py: reading gists for given user, save to file in progress, .gitignore: ignoring state file

--- gist-reader.py
+++ gist-reader.py
@@ -4,48 +4,85 @@ gist-reader.py: Reads gist for given user
 
 from multiprocessing import context
 import click
+import datetime
 from github import Github
+import json
+import os
 
 github_client = None
 github_user = None
+state_file = "last_run.json"
 
 @click.group()
-@click.option('--user', '-u', 'user', required=True,
-    help="GitHub username whose gists will be listed (Username:<name>)")
-def cli(user):
+def cli():
     """List available gists"""
     global github_client
-    global github_user
-
     github_client = Github()
-    github_user = user
+    # github_user = user
     
 @cli.command()
-def list():
+@click.option('--since-last-run', '-r', 'since_last_run', is_flag=True, show_default=True, 
+    required=False, default=True, help="Lists gists created since last local execution")
+@click.option('--user', '-u', 'user', required=True,
+    help="Specify GitHub username with public gists")
+@click.option('--since', '-s', 'since', required=False,
+    help=f"Warning! Ignored if --since-last-run flag is 'True'. List of gists published since given datetime, \n" + \
+    "(Format: '2022-09-26T14:54:54Z')")    
+
+def list(user, since, since_last_run):
     """list User gists"""
-    print(context)
-    user = "JoeyBurzynski"
-    user_gists = get_gists(github_user)
+    user_gists = get_gists(user, since, since_last_run)
 
-    print(f"Number of gists for %s: %i", user, len(user_gists))
+    print(f'Number of gists for {user}: {len(user_gists)}')
     for gist in user_gists:
-        print(gist)
+        print(f"Id: {gist.id}, Date created: {gist.created_at}, Description: {gist.description}")
 
-def get_gists(user):
+def get_gists(user, since, since_last_run):
     """Read user gists"""
+    # User: JoeyBurzynski
     user_gists = []
     page = 0
+    since = datetime.datetime.strptime(since, "%Y-%m-%dT%H:%M:%SZ")
 
     while True:
-        resp = github_client.get_user(user).get_gists().get_page(page)
+        resp = github_client.get_user(user).get_gists(since=since).get_page(page)
         if resp != []:
             user_gists += resp
             page += 1
         else:
-            print("Full list of gists.")
+            print(f"Pulled list of gists since {since}")
             break
     
+    write_last_run(user, datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"))
+
     return user_gists
+
+def write_last_run(user, current_datetime):
+    """Save latest execution details"""
+    # global state_file
+    data = {}
+
+    if os.path.exists(state_file):
+        with open(state_file, 'r', encoding='utf-8') as f:
+            data = json.loads(f.read())
+    
+    data[user] = current_datetime
+
+    with open(state_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def read_last_run(user):
+    """Read latest execution details"""
+    data = {}
+
+    if os.path.exists(state_file):
+        with open(state_file, 'r', encoding='utf-8') as f:
+            data[user] = json.loads(f.read())[user]
+    
+    return data
+
+def gist_details():
+    """Get gist details"""
 
 if __name__ == '__main__':
     cli()
