dd0f8648c502aff16fe8bca04907ac86f3b5ee42
Jaroslaw Dobromilski <jaroslaw.dobromilski@gmail.com>
2023-02-11 21:10:20 +0000

    added token authentication, fixes, lint

--- gist_reader.py
+++ gist_reader.py
@@ -8,35 +8,35 @@ import os
 import click
 from github import Github, GithubException, GithubObject
 
-state_file = "last_run.json"
-datetime_format = "%Y-%m-%dT%H:%M:%SZ"
+STATE_FILE = "last_run.json"
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+GITHUB_CLIENT = None
 
 
 @click.group()
+@click.option('--auth-token', '-t', 'auth_token', required=False,
+              default=lambda: os.environ.get("GISTS_AUTH_TOKEN", None),
+              help="Authentication token, gets environment variable GISTS_AUTH_TOKEN")
+def cli(auth_token):
+    """Read user gists"""
+    global GITHUB_CLIENT
+
+    GITHUB_CLIENT = Github(auth_token)
+
+
+@cli.command('get')
 @click.option('--user', '-u', 'user', required=True,
               help="Specify GitHub username with public gists")
 @click.option('--since-last-run', '-r', 'since_last_run', is_flag=True, show_default=False,
               required=False, default=False, help="Lists gists created since last local execution")
-@click.option('--since-given-datetime', '-s', 'since_given_datetime', required=False,
-              help="Warning! Ignored if --since-last-run flag is 'True'. List of gists published since " +
-              "given datetime, (Format: '2022-09-26T14:54:54Z')")
-@click.pass_context
-def cli(ctx, user, since_last_run, since_given_datetime):
-    """Read user gists"""
-    ctx.ensure_object(dict)
-    ctx.obj['USER'] = user
-    ctx.obj['SINCE_GIVEN_DATETIME'] = since_given_datetime
-    ctx.obj['SINCE_LAST_RUN'] = since_last_run
-
-
-@cli.command('list')
-@click.pass_context
-def list(ctx):
+@click.option('--since-given-datetime', '-s', 'since_given_datetime', required=False, default=None,
+              help="Warning! Ignored if --since-last-run flag is 'True'. List of gists published " +
+              "since given datetime, (Format: '2022-09-26T14:54:54Z')")
+def get(user, since_last_run, since_given_datetime):
     """Gists for given user"""
-    user_gists = get_gists(
-        ctx.obj['USER'], ctx.obj['SINCE_LAST_RUN'], ctx.obj['SINCE_GIVEN_DATETIME'])
+    user_gists = get_gists(user, since_last_run, since_given_datetime)
 
-    print(f"Number of gists for {ctx.obj['USER']}: {len(user_gists)}")
+    print(f"Number of gists for {user}: {len(user_gists)}")
     for gist in user_gists:
         print(
             f"Id: {gist.id}, Date created: {gist.created_at}, Description: {gist.description}")
@@ -44,23 +44,25 @@ def list(ctx):
 
 def get_gists(user, since_last_run, since_given_datetime):
     """Read user gists"""
-    # User: JoeyBurzynski
-    github_client = Github()
+    # User: jdobromilski
+
     user_gists = []
     page = 0
 
-    if since_last_run == True:
-        last_run = read_last_execution_time(user)
-        print(f"Last execution time: {last_run}")
+    if since_last_run is True:
+        since_when = read_last_execution_time(user)
+    elif since_given_datetime is not None:
+        since_when = datetime.datetime.strptime(
+            since_given_datetime, DATETIME_FORMAT)
     else:
-        last_run = GithubObject.NotSet
+        since_when = GithubObject.NotSet
 
     while True:
         try:
-            resp = github_client.get_user(
-                user).get_gists(last_run).get_page(page)
-        except GithubException as e:
-            raise Exception(1, f"Error pulling gists: {e.data['message']}")
+            resp = GITHUB_CLIENT.get_user(
+                user).get_gists(since_when).get_page(page)
+        except GithubException as exc:
+            raise exc
 
         if resp != []:
             user_gists += resp
@@ -75,17 +77,16 @@ def get_gists(user, since_last_run, since_given_datetime):
 
 def write_current_execution_time(user):
     """Save latest execution details"""
-    # global state_file
     data = {}
-    current_datetime = datetime.datetime.now().strftime(datetime_format)
+    current_datetime = datetime.datetime.now().strftime(DATETIME_FORMAT)
 
-    if os.path.exists(state_file):
-        with open(state_file, 'r', encoding='utf-8') as file:
+    if os.path.exists(STATE_FILE):
+        with open(STATE_FILE, 'r', encoding='utf-8') as file:
             data = json.loads(file.read())
 
     data[user] = current_datetime
 
-    with open(state_file, 'w', encoding='utf-8') as file:
+    with open(STATE_FILE, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
@@ -93,11 +94,11 @@ def read_last_execution_time(user):
     """Read latest execution details"""
     data = {}
 
-    if os.path.exists(state_file):
-        with open(state_file, 'r', encoding='utf-8') as file:
+    if os.path.exists(STATE_FILE):
+        with open(STATE_FILE, 'r', encoding='utf-8') as file:
             data = json.loads(file.read())[user]
 
-    data = datetime.datetime.strptime(data, datetime_format)
+    data = datetime.datetime.strptime(data, DATETIME_FORMAT)
 
     return data
 
